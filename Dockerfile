3caf8189b573b37f85c2a18337e84aa009604b7d
eltoncasacio <eltoncasacio@hotmail.com>
2023-07-18 01:50:26 +0000

    abstract value object test

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,13 @@
 FROM node:14.15.4-slim
 
-RUN echo "deb http://archive.debian.org/debian stretch main contrib non-free" > /etc/apt/sources.list
+RUN mkdir -p /usr/share/man/man1 && \
+    echo "deb http://archive.debian.org/debian stretch main contrib non-free" > /etc/apt/sources.list && \
+    apt update && apt upgrade && apt install -y \
+    git \
+    ca-certificates \
+    default-jre
 
-RUN apt update && apt install -y --no-install-recommends git
+# ENV JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64"
 
 USER node
 
--- src/@seedwork/domain/value-objects/__tests__/value-object.spec.ts
+++ src/@seedwork/domain/value-objects/__tests__/value-object.spec.ts
@@ -1,3 +1,22 @@
-describe('ValueObject Unit Tests', () => {
+import ValueObject from "../value-object";
 
+class StubValueObject extends ValueObject { }
+
+describe('ValueObject Unit Tests', () => {
+    it('should set value', () => {
+        let vo = new StubValueObject('string value')
+        expect(vo.value).toBe('string value')
+
+        vo = new StubValueObject({ prop1: 'value1' })
+        expect(vo.value).toStrictEqual({ prop1: 'value1' })
+    })
+
+    it('should convert to string', () => {
+        let vo = new StubValueObject(null)
+        expect(vo + "").toBe("null")
+
+        vo = new StubValueObject(undefined)
+        expect(vo + "").toBe("undefined")
+
+    })
 })
